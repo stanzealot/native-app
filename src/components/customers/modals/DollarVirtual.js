f9aacf15d8db48314fbc8908c1c4f9b446516ef7
stanzealot <stanzealot@gmail.com>
2023-10-18 22:01:11 +0100

    updated

--- src/components/customers/modals/DollarVirtual.js
+++ src/components/customers/modals/DollarVirtual.js
@@ -29,30 +29,60 @@ function DollarVirtual({setIsDollarVirtual,setIsDollarAmount,setIsBalanceOpen,se
                 </div>
 
                 <div className="name">
-                    <p className="title">Account name</p>
-                    <p className="sub-title">Flick <img src={process.env.PUBLIC_URL + "/images/modal-copy-icon.svg"} alt="icon" /></p>
+                    <p className="title">Routing Number</p>
+                    <p className="sub-title">091311229 <img src={process.env.PUBLIC_URL + "/images/modal-copy-icon.svg"} alt="icon" /></p>
                 </div>
             </div>
 
             <div className="number-name">
                 <div className="number">
                     <p className="title">Bank name</p>
-                    <p className="sub-title">Wema Bank <img src={process.env.PUBLIC_URL + "/images/modal-copy-icon.svg"} alt="icon" /></p>
+                    <p className="sub-title">Choice Financial Group  <img src={process.env.PUBLIC_URL + "/images/modal-copy-icon.svg"} alt="icon" /></p>
                 </div>
 
                 <div className="name">
                     <p className="title">Account type</p>
-                    <p className="sub-title">Current <img src={process.env.PUBLIC_URL + "/images/modal-copy-icon.svg"} alt="icon" /></p>
+                    <p className="sub-title">Checking <img src={process.env.PUBLIC_URL + "/images/modal-copy-icon.svg"} alt="icon" /></p>
                 </div>
             </div>
 
+
+            {/*  */}
+            <div className="number-name">
+                <div className="number">
+                    <p className="title">Bank address</p>
+                    <p className="sub-title">4501 23rd Avenue S,<br/> Fargo, ND 58104</p>
+                </div>
+
+                <div className="name">
+                    <p className="title">Beneficiary address</p>
+                    <p className="sub-title">
+                    2261 Market Street, #4664<br/> San Francisco, CA 94114
+                    </p>
+                </div>
+            </div>
+
+
+
+            <div className="number-name">
+                <div className="number">
+                    <p className="title">Narration</p>
+                    <p className="sub-title">Flick - Tx</p>
+                </div>
+
+              
+            </div>
+            
+                
+            
+
         </div>
 
-        <div className="modal-btn">
-          <button onClick={handleCancle} className="first-btn">
-           Cancel
+        <div className="modal-btn" style={{alignItems:"center"}}>
+          <button onClick={handleCancle} className="first-btn" style={{width:"47%",padding:"14px"}}>
+          Wire Details
           </button>
-          <button onClick={setIsDollarSuccess.bind(this,true)} className="btn">
+          <button onClick={setIsDollarSuccess.bind(this,true)} className="btn" style={{width:"48%",padding:"14px"}}>
             I've made payment
           </button> 
           
